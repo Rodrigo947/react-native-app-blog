129b61007426f99468a297cb3d0125cb5d1575d9
Rodrigo Torres <rodrigo.gviva@gmail.com>
2022-07-25 10:30:30 -0300

    pagina de search

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,23 +1,77 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, TextInput, StyleSheet, TouchableOpacity } from "react-native";
+
+import { Feather } from "@expo/vector-icons";
+
+import api from "../../services/api";
 
 export default function Search() {
+  const [input, setInput] = useState("");
+
+  async function handleSearchPost() {
+    if (input === "") {
+      alert("Digite algum nome!");
+      return;
+    }
+
+    const response = await api.get(
+      `/api/posts?filters[title][$contains]=${input}&populate=cover`
+    );
+
+    console.log(response.data?.data);
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Campo Search</Text>
+      <View style={styles.containerInput}>
+        <TextInput
+          style={styles.input}
+          placeholder="O que está buscando?"
+          onChangeText={(text) => setInput(text)}
+        />
+
+        <TouchableOpacity
+          style={styles.searchButton}
+          onPress={handleSearchPost}
+        >
+          <Feather name="search" size={25} color="#000" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#121212",
+    backgroundColor: "#FFF",
     flex: 1,
+    padding: 18,
+  },
+  containerInput: {
+    flexDirection: "row",
+    width: "100%",
+    height: 45,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
+  },
+  input: {
+    width: "85%",
+    backgroundColor: "#C4C4C4",
+    height: 45,
+    borderTopLeftRadius: 4,
+    borderBottomLeftRadius: 4,
+    padding: 8,
+    fontSize: 16,
+  },
+  searchButton: {
+    width: "15%",
     justifyContent: "center",
     alignItems: "center",
-  },
-  title: {
-    color: "#FFF",
-    fontSize: 24,
+    backgroundColor: "#C4C4C4",
+    height: 45,
+    borderTopRightRadius: 4,
+    borderBottomRightRadius: 4,
+    marginLeft: -1,
   },
 });
